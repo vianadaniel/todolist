f2c7d0c822e0a88e04118756486824444316f41e
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2020-06-22 16:03:30 -0300

    Dates working

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 const express = require("express");
-cont bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.render("Hello");
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Day wrong" + currentDay);
+            
+    }
+    
+    
+    res.render("list", {
+        kindOfDay: day
+    });
+
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server on 3000");
-    
+
 });
