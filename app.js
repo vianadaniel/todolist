f8b6254759038bad29f71c6ae866bb12431623b4
Daniel <daniel@MacBook-Pro-de-Daniel.local>
2020-06-22 17:15:06 -0300

    Date better

--- app.js
+++ app.js
@@ -9,35 +9,13 @@ app.get("/", function (req, res) {
 
     var today = new Date();
     var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Day wrong" + currentDay);
-            
-    }
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options)
     
     
     res.render("list", {
